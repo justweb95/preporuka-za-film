9ef99d798bffb2ab2859e16b0ac3ec74a51915f1
justweb <justweb.rs@gmail.com>
2024-12-10 01:15:00 +0100

    Initial Commit

---
+++ resources/views/template-home.blade.php
@@ -0,0 +1,11 @@
+{{--
+  Template Name: Home Page Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  <h1>Pavle Sage 10.8.1</h1>
+  <h3 class="add-text"></h3>
+  <button id="button">Clickni me</button>
+@endsection
